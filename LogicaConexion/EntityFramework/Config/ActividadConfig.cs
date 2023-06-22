e50bd71daab6d49d4d6d98ba57463f1f099ba480
Felipe Macedo Costa <macedofelipecosta@users.noreply.github.com>
2023-06-22 20:09:27 -0300

    Se eliminan using inecesarios de la clase ActividadConfig en EF

--- LogicaConexion/EntityFramework/Config/ActividadConfig.cs
+++ LogicaConexion/EntityFramework/Config/ActividadConfig.cs
@@ -1,12 +1,6 @@
 ﻿using LogicaNegocio.Entidades.Actividades;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LogicaConexion.EntityFramework.Config
 {
