2735ce367ee2153156c1a7da9b68acfe18af50bb
Felipe Macedo Costa <128637344+macedofelipecosta@users.noreply.github.com>
2023-06-28 15:01:13 -0300

    Modificaciones en Logica Aplicacion Administradores, se continuan los cambios

---
+++ LogicaAplicacion/CasosUso/Administradores/DeleteAdministrador.cs
@@ -0,0 +1,14 @@
+﻿using LogicaAplicacion.Interfaces;
+using LogicaNegocio.Entidades;
+
+
+namespace LogicaAplicacion.CasosUso.Administradores
+{
+    public class DeleteAdministrador : DeleteObject<Administrador>
+    {
+        public void DeleteObj(Administrador obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LogicaNegocio/Entidades/Administrador.cs
+++ LogicaNegocio/Entidades/Administrador.cs
@@ -1,9 +1,4 @@
 ﻿using LogicaNegocio.ValueObject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LogicaNegocio.Entidades
 {
