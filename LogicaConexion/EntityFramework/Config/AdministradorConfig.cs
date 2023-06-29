e083016976fe4b29689d3b5dc14b50fb626020ef
Felipe Macedo Costa <macedofelipecosta@users.noreply.github.com>
2023-06-28 21:18:24 -0300

    Se empezo a crear las lases config en entityFramework

---
+++ LogicaConexion/EntityFramework/Config/AdministradorConfig.cs
@@ -0,0 +1,24 @@
+﻿using LogicaNegocio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaConexion.EntityFramework.Config
+{
+    internal class AdministradorConfig : IEntityTypeConfiguration<Administrador>
+    {
+        public void Configure(EntityTypeBuilder<Administrador> builder)
+        {
+            builder.HasKey("Id");
+            builder.HasIndex(e => e.Id).IsUnique();
+
+            builder.OwnsOne(administrador => administrador.Name.Data).Property(Name=>Name).HasColumnName("NombreAdmin");
+            builder.OwnsOne(administrador => administrador.Email.Data).Property(Email => Email).HasColumnName("EmailAdmin");
+            //builder.OwnsOne(administrador => administrador.Identificador).Property(Identificador => Identificador).HasColumnName("EmailAdmin");
+        }
+    }
+}
